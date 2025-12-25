9b6b5b42977255824656c3321743d88a038f25cd
Sazid Ahmed Nassir <sazidahmednassir@gmail.com>
2025-12-25 13:44:04 +0600

    Pratice js variables

--- basic.js
+++ basic.js
@@ -1,1 +1,29 @@
 console.log("This is a basic JavaScript file.");
+
+let greeting = "Hello, World!";
+console.log(`From Sazid : ${greeting}`);
+
+console.log(typeof greeting);
+
+// let variable cannot be re-declared but can be re-assigned value
+let number = 24;
+console.log(`The number is: ${number}`);
+
+let required = true;
+console.log(`Data type : ${required}`);
+
+let a = 10;
+let b = 20;
+
+//var variable can be re-declared and re-assigned value
+var c = a + b;
+console.log(`The sum of a and b is: ${c}`);
+
+var c = "Nassir";
+console.log(`My name is: ${c}`);
+
+console.log(!required);
+
+//const variable cannot be re-declared or re-assigned value
+const pi = 3.1416;
+console.log(`Value of pi: ${pi}`);
