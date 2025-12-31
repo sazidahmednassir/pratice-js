cc14f3214d6ff2f1f3e913a8945f1c0de2e02754
sazidahmednassir <sazidahmednassir@gmail.com>
2025-12-31 17:00:37 +0600

    for rebase pratice

---
+++ devopsei1.js
@@ -0,0 +1,8 @@
+//do…while — used when you must do the task at least once, and then repeat it again based on the condition.
+do {
+  console.log("This will run at least once.");
+  count--;
+} while (count > 0);
+{
+  console.log("Count is less than 0" + count);
+}
